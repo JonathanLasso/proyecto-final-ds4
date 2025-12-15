791395942372b35d5cb2ffd7c2a234d838f98874
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-12-15 16:27:32 -0500

    Se implemento todo el desarrollo de la aplicación web para agendar tareas de manera semanal

---
+++ proyectoFinalDs4/proyectoFinalDs4/Controllers/AgendarTareasController.cs
@@ -0,0 +1,97 @@
+﻿using proyectoFinalDs4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyectoFinalDs4.Controllers
+{
+    public class AgendarTareasController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.lista = Agendar.todasLasTareasAgendadas();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CrearTarea()
+        {
+            return View(new Agendar());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CrearTarea(Agendar tarea)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tarea); // vuelve a la misma vista
+            }
+            try
+            {
+                tarea.insertarTarea(); // usa tu método del modelo
+                TempData["mensaje"] = "Tarea agendada correctamente";
+                return RedirectToAction("Index"); // o mostrarListaTareas
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(tarea);
+            }
+        }
+        [HttpGet]
+        public ActionResult EditarTarea(int id)
+        {
+            Agendar modelo = Agendar.ObtenerPorId(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditarTarea(Agendar tarea)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tarea);
+            }
+            try
+            {
+                tarea.actualizarTarea();
+                TempData["mensaje"] = "Tarea actualizada correctamente";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Ocurrió un error al actualizar la tarea.\"");
+                return View(tarea);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Eliminar(int id)
+        {
+            Agendar tarea = new Agendar();
+            tarea.eliminarTarea(id);
+
+            TempData["mensaje"] = "Tarea eliminada correctamente";
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult EliminarTodas()
+        {
+            Agendar tarea = new Agendar();
+            tarea.eliminarTodasLasTareas();
+
+            TempData["mensaje"] = "Todas las tareas fueron eliminadas";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ proyectoFinalDs4/proyectoFinalDs4/Models/Agendar.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace proyectoFinalDs4.Models
+{
+    public class Agendar
+    {
+        public int idTarea { set; get; }
+        [Display(Name = "Título de la tarea")]
+        [Required(ErrorMessage = "El título es obligatorio")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string titulo { set; get; }
+        [Display(Name = "Descripción")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string descripcion { set; get; }
+        [Display(Name = "Dia de la semana")]
+        [Required(ErrorMessage = "Debe seleccionar un día")]
+        [Range(1, 7, ErrorMessage = "Seleccione un día válido")]
+        public int diaSemana { set; get; }
+        [Display(Name = "Hora de inicio")]
+        [Required(ErrorMessage = "La hora de inicio es obligatoria")]
+        public TimeSpan horaInicio { set; get; }
+        [Display(Name = "Hora de finalización")]
+        [Required(ErrorMessage = "La hora de fin es obligatoria")]
+        public TimeSpan horaFin {  set; get; }
+        [Display(Name = "¿Tarea completada?")]
+        public bool completada { set; get; }
+
+        ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["ConexionAgendarTarea"];
+        SqlConnection conexion = new SqlConnection();
+
+        public void abrirConexion()
+        {
+            try
+            {
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.ConnectionString = connString.ConnectionString;
+                    conexion.Open();
+                }
+
+            }
+            catch (Exception) { throw new Exception("Error al abrir la conexion"); }
+        }
+        public void cerrarConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Open)
+                    conexion.Close();
+
+            }
+            catch (Exception) { throw new Exception("Error al cerrar la conexion"); }
+
+        }
+
+        public static List<Agendar> todasLasTareasAgendadas()
+        {
+            string consulta = "SELECT IdTarea, Titulo, Descripcion, DiaSemana, HoraInicio, HoraFin, TareaCompletada FROM TAREA ORDER BY IdTarea DESC";
+            return obtenerListasTareas(consulta);
+        }
+
+        public static Agendar ObtenerPorId(int idTarea)
+        {
+            Agendar tarea = null;
+            Agendar conexionTarea = new Agendar();
+
+            try
+            {
+                conexionTarea.abrirConexion();
+
+                SqlCommand comando = new SqlCommand(
+                    "SELECT IdTarea, Titulo, Descripcion, DiaSemana, HoraInicio, HoraFin, TareaCompletada " +
+                    "FROM TAREA WHERE IdTarea = @idTarea", conexionTarea.conexion);
+
+                comando.Parameters.AddWithValue("@idTarea", idTarea);
+
+                SqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    tarea = new Agendar
+                    {
+                        idTarea = Convert.ToInt32(reader["IdTarea"]),
+                        titulo = reader["Titulo"].ToString(),
+                        descripcion = reader["Descripcion"].ToString(),
+                        diaSemana = Convert.ToInt32(reader["DiaSemana"]),
+                        horaInicio = (TimeSpan)reader["HoraInicio"],
+                        horaFin = (TimeSpan)reader["HoraFin"],
+                        completada = Convert.ToBoolean(reader["TareaCompletada"])
+                    };
+                }
+
+                conexionTarea.cerrarConexion();
+                return tarea;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener la tarea por ID: " + ex.Message);
+            }
+        }
+
+
+        public static List<Agendar> obtenerListasTareas(string consulta)
+        {
+            List<Agendar> agendasTareas = new List<Agendar>();
+            Agendar tarea = new Agendar();
+            try
+            {
+                tarea.abrirConexion();
+                SqlCommand comando = new SqlCommand(consulta, tarea.conexion);
+                SqlDataReader leerDatos = comando.ExecuteReader();
+                while (leerDatos.Read())
+                {
+                    agendasTareas.Add(new Agendar
+                    {
+                        idTarea = Convert.ToInt32(leerDatos["IdTarea"]),
+                        titulo = leerDatos["Titulo"].ToString(),
+                        descripcion = leerDatos["Descripcion"].ToString(),
+                        diaSemana = Convert.ToInt32(leerDatos["DiaSemana"]), // tipo de dato int
+                        horaInicio = (TimeSpan)leerDatos["HoraInicio"], // tipo de dato TimeSpan
+                        horaFin = (TimeSpan)leerDatos["HoraFin"], // tipo de dato TimeSpan
+                        completada = Convert.ToBoolean(leerDatos["TareaCompletada"]) // tipo de dato booleano
+                    });
+                }
+                tarea.cerrarConexion();
+                return agendasTareas;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: " + ex.ToString());
+            }
+        }
+        public void insertarTarea()
+        {
+            try
+            {
+                abrirConexion();
+                SqlCommand comando = new SqlCommand("INSERT INTO TAREA (Titulo, Descripcion, DiaSemana, HoraInicio, HoraFin, TareaCompletada) " +
+                    "VALUES (@titulo, @descripcion, @diaSemana, @horaInicio, @horaFin, @tareaCompletada)", conexion);
+                comando.Parameters.AddWithValue("@titulo", titulo);
+                comando.Parameters.AddWithValue("@descripcion", descripcion);
+                comando.Parameters.AddWithValue("@diaSemana", diaSemana);
+                comando.Parameters.AddWithValue("@horaInicio", horaInicio);
+                comando.Parameters.AddWithValue("@horaFin", horaFin);
+                comando.Parameters.AddWithValue("@tareaCompletada", completada);
+                comando.ExecuteNonQuery();
+                cerrarConexion();
+            }
+            catch (Exception) { throw new Exception("Error al insertar una tarea"); }
+        }
+        public void actualizarTarea()
+        {
+            try
+            {
+                abrirConexion();
+                SqlCommand comando = new SqlCommand(
+                    "UPDATE TAREA SET " +
+                    "Titulo = @titulo, " +
+                    "Descripcion = @descripcion, " +
+                    "DiaSemana = @diaSemana, " +
+                    "HoraInicio = @horaInicio, " +
+                    "HoraFin = @horaFin, " +
+                    "TareaCompletada = @tareaCompletada " +
+                    "WHERE IdTarea = @idTarea", conexion);
+                comando.Parameters.AddWithValue("@idTarea", idTarea);
+                comando.Parameters.AddWithValue("@titulo", titulo);
+                comando.Parameters.AddWithValue("@descripcion", descripcion);
+                comando.Parameters.AddWithValue("@diaSemana", diaSemana);
+                comando.Parameters.AddWithValue("@horaInicio", horaInicio);
+                comando.Parameters.AddWithValue("@horaFin", horaFin);
+                comando.Parameters.AddWithValue("@tareaCompletada", completada);
+                comando.ExecuteNonQuery();
+                cerrarConexion();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al actualizar la tarea");
+            }
+        }
+        public void eliminarTarea(int idTarea)
+        {
+            try
+            {
+                abrirConexion();
+                SqlCommand comando = new SqlCommand(
+                    "DELETE FROM TAREA WHERE IdTarea = @idTarea", conexion);
+                comando.Parameters.AddWithValue("@idTarea", idTarea);
+                comando.ExecuteNonQuery();
+                cerrarConexion();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al eliminar la tarea");
+            }
+        }
+        public void eliminarTodasLasTareas()
+        {
+            try
+            {
+                abrirConexion();
+                SqlCommand comando = new SqlCommand(
+                    "DELETE FROM TAREA", conexion);
+                comando.ExecuteNonQuery();
+                cerrarConexion();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al eliminar todas las tareas");
+            }
+        }
+        public static Agendar agendarTarea(string titulo, string descripcion, int diaSemana, TimeSpan horaInicio, TimeSpan horaFin, bool tareaCompletada)
+        {
+            Agendar tarea = new Agendar();
+            tarea.titulo = titulo;
+            tarea.descripcion = descripcion;
+            if(diaSemana >= 1 && diaSemana <= 7)
+            {
+                tarea.diaSemana = diaSemana;
+                tarea.horaInicio = horaInicio;
+                tarea.horaFin = horaFin;
+                tarea.completada = tareaCompletada;
+            }
+            else
+            {
+                throw new Exception("El día de la semana debe estar entre 1 y 7");
+            }
+            try
+            {
+                tarea.insertarTarea();
+                return tarea;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: " + ex.ToString());
+            }
+        }
+
+    }
+}
